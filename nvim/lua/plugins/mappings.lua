4a498ef734629486e95791dc3ae0d2de1b7e15d7
Jake Hayes <jake.hayes@mantl.com>
2025-01-17 16:53:49 -0500

    Live grep added

--- nvim/lua/plugins/mappings.lua
+++ nvim/lua/plugins/mappings.lua
@@ -5,7 +5,11 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<Leader>a"] = { desc = "Add to Harpoon" },
+          ["<leader>a"] = { desc = "Add to Harpoon" },
+          ["<leader>fw"] = {
+            "<cmd> Telescope live_grep<cr>",
+            desc = "Find words in all files",
+          },
         },
       },
     },
